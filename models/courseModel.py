5d6985184edd90e2c904e679dc24809781204b3c
hanzacosta <158714271+hanzacosta@users.noreply.github.com>
2026-01-24 19:04:20 +0800

    Update courseModel.py

--- models/courseModel.py
+++ models/courseModel.py
@@ -1,1 +1,28 @@
+from config.database import get_connection
+
+class Course:
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+
+    @staticmethod
+    def get_all():
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM courses")
+        courses = [Course(row['id'], row['name'], row['description']) for row in cursor.fetchall()]
+        conn.close()
+        return courses
+
+    @staticmethod
+    def get_by_id(course_id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM courses WHERE id=?", (course_id,))
+        row = cursor.fetchone()
+        conn.close()
+        if row:
+            return Course(row['id'], row['name'], row['description'])
+        return None
 
